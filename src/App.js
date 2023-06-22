8a13ad3d4133099778a422446b99bed5ca62b550
Ryan Li <rwli@usc.edu>
2023-06-22 14:15:53 -0700

    front end ui + hook to flask

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([{}])
 
   useEffect(() =>{
-    fetch("/members").then(
+    fetch("/similar").then(
       res => res.json()
     ).then(
       data => {
@@ -15,20 +15,52 @@ function App() {
     )
   }, []
   )
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const form = e.target
+    const formData = new FormData(form);
+    fetch('http://localhost:5000/prospect', { method: form.method, body: formData })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <p>Player Success Predictor</p>
+        <h2>Player Success Predictor</h2>
         
       </header>
-      <h2>Common Predictions:</h2>
+      <body className='App-body'>
+      
+        <p>Enter the Name, Position, RAS <a target="_blank" and rel="noopener noreferrer" href="https://ras.football/ras-calculator/">(Raw Athletic Score)</a>, and College of a prospect and we will predict their future success in the NFL! </p>
+        <form method="post" onSubmit={handleSubmit} className='form'>
+          <label> Name: <input name = "playerName" defaultValue="Tom Brady" /></label>
+          <label> College:  <input name = "college" defaultValue="University of Southern California" /> </label>
+          <label> Position: 
+          <select name="position">
+          <option value="QB">QB</option>
+          <option value="RB">RB</option>
+		      </select>
+          </label>
+          <label> RAS: <input name = "ras" defaultValue="10.0" /></label>
+          <button type="submit">Compare Player!</button>
+        </form>
+      </body>
+      <footer className='App-footer'>
+      <h3>Common Predictions:</h3>
         {(typeof data.members === 'undefined') ? (
-          <p>1 sec....</p>
+          <p>Loading.....</p>
         ) : (
           data.members.map((member, i) => (
             <p key ={i}>{member}</p>
           ))
         )}
+      </footer>
     </div>
   );
 }
